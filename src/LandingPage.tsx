d41a7a00bd454a317a71c38e04a95808bdb07b93
Matthew Fastow <55714940+mattfast@users.noreply.github.com>
2023-10-04 09:41:45 -0700

    better copy for landing page

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -113,7 +113,7 @@ const LandingPage = () => {
                 <div className="name">dopple.club</div>
               </div>
               <div className="subtitle">
-                <div className="pink">Create</div> a dopple.<div className="pink">Vote</div> for friends. <div className="pink">Win</div> prizes.
+                <div className="pink">Sign up</div> to <div className="pink">compete</div> against <div className="pink">your</div> friends
               </div>
             </div>
             <LandingPageImage />
@@ -126,7 +126,7 @@ const LandingPage = () => {
                 }
               >
                 <div className="buttonText">
-                  Compete Now
+                  Create your Dopple
                 </div>
               </div>
             </div>
