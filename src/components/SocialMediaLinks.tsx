97a507f76f25ba1cdf28fa560584fc653c35731e
Matthew Fastow <55714940+mattfast@users.noreply.github.com>
2023-10-25 13:31:04 -0700

    change social links

--- src/components/SocialMediaLinks.tsx
+++ src/components/SocialMediaLinks.tsx
@@ -7,16 +7,16 @@ const SocialMediaLinks = () => {
  
   return (
     <div className="socialMediaContainer">
-      <div className="linkWidget" onClick={() => window.open("https://www.tiktok.com/@milk__ai/", "_blank")}>
+      <div className="linkWidget" onClick={() => window.open("https://www.tiktok.com/@fitpix_io/", "_blank")}>
         <Image src="social_media/tiktok.png" width="24px" height="24px" />
       </div>
       <div className="linkWidget" onClick={() => window.open("https://discord.gg/mgWxBhAk", "_blank")}>
         <Image src="social_media/discord.png" width="24px" height="20px" />
       </div>
-      <div className="linkWidget" onClick={() => window.open("https://www.instagram.com/paly_dopple/", "_blank")}>
+      <div className="linkWidget" onClick={() => window.open("https://www.instagram.com/fitpix_io/", "_blank")}>
         <Image src="social_media/instagram.png" width="24px" height="24px" />
       </div>
-      <div className="linkWidget" onClick={() => window.open("https://twitter.com/milk_ai__", "_blank")}>
+      <div className="linkWidget" onClick={() => window.open("https://twitter.com/fitpix_io", "_blank")}>
         <Image src="social_media/twitter.png" width="24px" height="24px" />
       </div>
     </div>
